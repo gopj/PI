20873f0983763e6252c6e4f8529635871a105edf
Jesús González <gopixc@gmail.com>
2013-04-20 17:33:58 -0500

    pruebas eliminadas

--- application/views/index.php
+++ application/views/index.php
@@ -14,24 +14,26 @@
     </div>
 	
 
-
+<!--
 	<div class="btn-group">
 	  <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
 	    Action
 	    <span class="caret"></span>
 	  </a>
 	  <ul class="dropdown-menu">
-	    <!-- dropdown menu links -->
+	   
 	  </ul>
 	</div>
 
+
+	
 	<ul class="nav nav-tabs">
 		<li class="active">
 			<a href="<?=base_url();?>">Crear Posts</a>
 		</li>
 
 		<li><a href="<?=base_url();?>posts/getPost/">Ver Posts</a></li>
-	</ul>
+	</ul> -->
 
 		
 	
