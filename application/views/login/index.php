cb5f6aeb1471886e569c0142380d7a4921c2da86
Angel Reynaga <aj_rdgz@hotmail.com>
2013-04-22 22:13:55 -0500

    Span3 to the inputs

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -2,9 +2,9 @@
 	<h2 class="form-signin-heading">Identificaci&oacute;n</h2>
 	<p>
 		<label for="usuario">Usuario: </label>
-		<input name="usuario" type="usuario" class="input-block-level" placeholder="Nombre de usuario">
+		<input name="usuario" type="usuario" class="input-block-level span3" placeholder="Nombre de usuario">
 		<label for="clave">Contrase&ntilde;a:</label>
-		<input name="clave" type="password" class="input-block-level" placeholder="Clave">
+		<input name="clave" type="password" class="input-block-level span3" placeholder="Clave">
 	</p>
 
 	<?php echo form_submit("login", "Entrar", 'class="btn btn-large btn-primary"'); ?>
