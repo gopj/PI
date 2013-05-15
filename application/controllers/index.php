3d5f8b5f90690df9a522536b59f61ad120fb3dbd
Jesús González <gopixc@gmail.com>
2013-05-15 13:25:52 -0500

    asdasdas

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -6,20 +6,11 @@ class Index extends My_Controller {
 		parent::__construct();
 		$this->setLayout('default');
 
-		$this->load->model("municipio_model", "municipio");
-		$this->load->model("cliente_model", "cliente");
 	}
 
 	public function index($id = true){
 
-		$municipio = new Municipio_model();
-		$cliente = new Cliente_model();
-
-
-		$data['municipios'] = $this->municipio->getAllToSelect();
-		$data['clientes'] = $this->cliente->getByIdToSelect($id);
-
-		$this->load->view('index/index', $data);
+		$this->load->view('index/index');
 	}
 
 	
