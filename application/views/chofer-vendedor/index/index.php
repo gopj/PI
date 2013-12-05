74682b1605badfd448dc97c15aee7d9f9c62c334
Jesús González <gopixc@gmail.com>
2013-12-05 16:57:14 -0600

    sistema actualizado

--- application/views/chofer-vendedor/index/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Esta es la vísta index de Chofer vendedor</h1>
-<p>Aqui va la info respectiva</p>
--- application/views/usuario-inventario/index/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Esta es la vísta index de usuario inventario</h1>
-<p>Aqui va la info respectiva</p>
